7d9becc16d086270edb4024c624cd4f077a1b863
Julia Parewick <Julia.Parewick37@ed.cna.nl.ca>
2022-08-09 21:29:53 -0230

    sound effects!

--- app/src/main/java/com/example/fakecallerapp/CallActivity.java
+++ app/src/main/java/com/example/fakecallerapp/CallActivity.java
@@ -2,15 +2,21 @@ package com.example.fakecallerapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.media.MediaPlayer;
+import android.provider.Settings;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CallActivity extends AppCompatActivity {
+    public MediaPlayer player;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.call_activity);
-        Intent intent = getIntent();
+        //sound
+        player = MediaPlayer.create(this, Settings.System.DEFAULT_RINGTONE_URI);
+        player.setLooping(true);
+        player.start();
     }
 }
