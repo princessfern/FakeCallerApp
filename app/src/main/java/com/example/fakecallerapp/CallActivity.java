623065abcf4f8a18eb790aaba71d9a97d768b888
Julia Parewick <Julia.Parewick37@ed.cna.nl.ca>
2022-08-10 21:05:36 -0230

    fragment class added, ringer now stops on pickup, call menu appears

--- app/src/main/java/com/example/fakecallerapp/CallActivity.java
+++ app/src/main/java/com/example/fakecallerapp/CallActivity.java
@@ -6,13 +6,16 @@ import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentContainerView;
 
 public class CallActivity extends AppCompatActivity {
     public MediaPlayer player;
     ImageButton accept;
     ImageButton decline;
+    FragmentContainerView frag;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,5 +31,9 @@ public class CallActivity extends AppCompatActivity {
         decline = findViewById(R.id.declineBtn);
         accept.setVisibility(view.INVISIBLE);
         decline.setVisibility(view.INVISIBLE);
+        player.stop();
+        getSupportFragmentManager().beginTransaction().add(R.id.container, new CallAcceptedFragment()).setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out).commit();
+        frag = findViewById(R.id.container);
+        frag.setVisibility(view.VISIBLE);
     }
 }
